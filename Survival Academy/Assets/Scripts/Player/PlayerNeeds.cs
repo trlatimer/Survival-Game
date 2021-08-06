575bd9b93a28584b0604d4b7e37112a3fbd1c13c
trlatimer <trlatimer95@gmail.com>
2021-08-05 17:23:27 -0700

    Implemented player need system, added a damaging environment object

--- Survival Academy/Assets/Scripts/Player/PlayerNeeds.cs
+++ Survival Academy/Assets/Scripts/Player/PlayerNeeds.cs
@@ -1,18 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
-public class PlayerNeeds : MonoBehaviour
+public class PlayerNeeds : MonoBehaviour, IDamagable
 {
-    // Start is called before the first frame update
-    void Start()
+    public Need health;
+    public Need hunger;
+    public Need thirst;
+    public Need sleep;
+
+    public float noHungerHealthDecay;
+    public float noThirstHealthDecay;
+
+    public UnityEvent onTakeDamage;
+
+    private void Start()
     {
-        
+        health.curValue = health.startValue;
+        hunger.curValue = hunger.startValue;
+        thirst.curValue = thirst.startValue;
+        sleep.curValue = sleep.startValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // decay needs over time
+        hunger.Subtract(hunger.decayRate * Time.deltaTime);
+        thirst.Subtract(thirst.decayRate * Time.deltaTime);
+        sleep.Add(sleep.regenRate * Time.deltaTime);
+
+        if (hunger.curValue == 0.0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+        if (thirst.curValue == 0.0f)
+        {
+            health.Subtract(noThirstHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue == 0.0f)
+        {
+            Die();
+        }
+
+        // Update UI Bars
+        health.uiBar.fillAmount = health.GetPercentage();
+        hunger.uiBar.fillAmount = hunger.GetPercentage();
+        thirst.uiBar.fillAmount = thirst.GetPercentage();
+        sleep.uiBar.fillAmount = sleep.GetPercentage();
     }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Sleep(float amount)
+    {
+        sleep.Subtract(amount);
+    }
+
+    public void TakePhysicalDamage(int amount)
+    {
+        health.Subtract(amount);
+        onTakeDamage?.Invoke();
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player is dead");
+    }
+}
+
+[System.Serializable]
+public class Need
+{
+    [HideInInspector]
+    public float curValue;
+    public float maxValue;
+    public float startValue;
+    public float regenRate;
+    public float decayRate;
+    public Image uiBar;
+
+    public void Add(float amount)
+    {
+        curValue = Mathf.Min(curValue + amount, maxValue);
+    }
+
+    public void Subtract(float amount)
+    {
+        curValue = Mathf.Max(curValue - amount, 0.0f);
+    }
+
+    public float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+}
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damageAmount);
 }
