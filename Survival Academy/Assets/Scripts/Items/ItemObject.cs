a50555bccf94d98e14dd256ffa125a6f0031cd06
Tyler Latimer <trlatimer95@gmail.com>
2021-08-08 12:43:32 -0700

    Added initial interactable system

--- Survival Academy/Assets/Scripts/Items/ItemObject.cs
+++ Survival Academy/Assets/Scripts/Items/ItemObject.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
+    public string GetInteractPrompt()
+    {
+        return string.Format("Pickup {0}", item.displayName);
+    }
+
+    public void OnInteract()
+    {
+        // TODO Add to inventory
+        Destroy(gameObject);
+    }
 }
---
+++ Survival Academy/Assets/Scripts/Player/InteractionManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractionManager : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+    public TextMeshProUGUI promptText;
+
+    private GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+    private float lastCheckTime;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        // true every "checkRate" seconds
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractable = null;
+                curInteractGameObject = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+    }
+
+}
+
+public interface IInteractable
+{
+    string GetInteractPrompt();
+    void OnInteract();
+}
