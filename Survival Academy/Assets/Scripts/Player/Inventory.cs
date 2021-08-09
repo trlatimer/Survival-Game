22572ba82b2e9afc088632627fa18d651a7401dc
Tyler Latimer <trlatimer95@gmail.com>
2021-08-08 22:45:49 -0700

    Added UI for Inventory, started Inventory scripts

---
+++ Survival Academy/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class Inventory : MonoBehaviour
+{
+    public ItemSlotUI[] uiSlots;
+    public ItemSlot[] slots;
+
+    public GameObject inventoryWindow;
+    public Transform dropPosition;
+
+    [Header("Selected Item")]
+    private ItemSlot selectedItem;
+    private int selectedItemIndex;
+    public TextMeshProUGUI selectedItemName;
+    public TextMeshProUGUI selectedItemDescription;
+    public TextMeshProUGUI selectedItemStatNames;
+    public TextMeshProUGUI selectedItemStatValues;
+    public GameObject useButton;
+    public GameObject equipButton;
+    public GameObject unEquipButton;
+    public GameObject dropButton;
+
+    private int curEquipIndex;
+
+    // Components
+    private PlayerController controller;
+
+    [Header("Events")]
+    public UnityEvent onOpenInventory;
+    public UnityEvent onCloseInventory;
+
+    // singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+        controller = GetComponent<PlayerController>();
+    }
+}
+
+public class ItemSlot
+{
+    public ItemData item;
+    public int quantity;
+}
